e5a072ccae285b66eb5fb18737538f530dd1c3e5
Limber <limachiquispelimber@gmail.com>
2026-06-24 17:20:14 -0400

    Solucion clave secreta XD pero no se com no subirlo

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SECRET_KEY = "LimberLimachiQuispe13645235"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database", "transporte.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
